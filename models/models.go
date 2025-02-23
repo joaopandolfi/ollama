d665582299b75e67e635d963b2b38bc526f20a9e
John McBirde <jpmmcbride@gmail.com>
2025-02-23 11:04:07 -0700

    feat: Move models package
    Signed-off-by: John McBirde <jpmmcbride@gmail.com>

--- models/models.go
+++ models/models.go
@@ -1,8 +1,13 @@
-package qwen
+package models
 
 import (
 	"github.com/agent-api/core/types"
 )
+
+// Deepseek models
+var DEEPSEEK_R1_7B *types.Model = &types.Model{
+	ID: "deepseek-r1:7b",
+}
 
 var QWEN2_5_LATEST *types.Model = &types.Model{
 	ID: "qwen2.5:latest",
